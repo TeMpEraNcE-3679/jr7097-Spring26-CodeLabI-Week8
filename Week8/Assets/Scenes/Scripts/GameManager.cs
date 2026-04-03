0c4089d01f275fb08c44727d343878a0d540d97b
TeMpEraNcE-3679 <jiayuruan315@gmail.com>
2026-04-03 02:34:01 -0400

    mechanics completed

--- Week8/Assets/Scenes/Scripts/GameManager.cs
+++ Week8/Assets/Scenes/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //This should be a detective game
+    //This should be a weird quiz
 
     //List of UI
     public TMP_Text Description;
@@ -15,20 +15,70 @@ public class GameManager : MonoBehaviour
     public Option CurrentOption;
     public Option StartOption;
 
-    public GameObject Option0;
-    public GameObject Option1;
-    public GameObject Option2;
+    public GameObject Option0Button;
+    public GameObject Option1Button;
+    public GameObject Option2Button;
+    public GameObject BackButton;
+
+    public static GameManager instance;
     
 
 
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Debug.Log("Current Option: " + StartOption);
+
+        StartOption.UpdateOptionDisplay(this);
+
+        CurrentOption = StartOption;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShiftOption(int Option)
+    {
+        switch(Option)
+        {
+            case 0:
+                if (CurrentOption.Option0 != null)
+                    CurrentOption.Option0.Back = CurrentOption;
+                    CurrentOption = CurrentOption.Option0;
+                break;
+
+            case 1:
+                if (CurrentOption.Option1 != null)
+                        CurrentOption.Option1.Back = CurrentOption;
+                        CurrentOption = CurrentOption.Option1;
+                break;
+
+            case 2:
+                if (CurrentOption.Option2 != null)
+                    CurrentOption.Option2.Back = CurrentOption;
+                    CurrentOption = CurrentOption.Option2;
+                break;
+
+            case 3:
+                if (CurrentOption.Back != null) 
+                    CurrentOption = CurrentOption.Back;
+                break;
+
+        }
+
+        CurrentOption.UpdateOptionDisplay(this);
     }
 }
--- Week8/Assets/Scenes/Scripts/Option.cs
+++ Week8/Assets/Scenes/Scripts/Option.cs
@@ -3,5 +3,36 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Option", menuName = "Scriptable Objects/Options")]
 public class Option : ScriptableObject
 {
-    
+    public string description;
+    public string Option0Text;
+    public string Option1Text;
+    public string Option2Text;
+
+    public Option Option0;
+    public Option Option1;
+    public Option Option2;
+    public Option Back;
+
+    //private void OnValidate()
+    //{
+    //    if (GameManager.instance != null)
+    //    { 
+    //     UpdateOptionDisplay(GameManager.instance);
+    //    }
+    //
+    //
+    //}
+
+    public void UpdateOptionDisplay(GameManager gm)
+    {
+        gm.Description.text = description;
+        gm.Text0.text = Option0Text;
+        gm.Text1.text = Option1Text;
+        gm.Text2.text = Option2Text;
+
+        gm.BackButton.SetActive(Back != null);
+        gm.Option0Button.SetActive(Option0 != null);
+        gm.Option1Button.SetActive(Option1 != null);
+        gm.Option2Button.SetActive(Option2 != null);
+    }
 }
